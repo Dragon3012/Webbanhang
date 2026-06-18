4e73a585c4112e7822098f8648ac24310fca267f
Nguyễn Xuân Trường <nguyentruong3012k3@gmail.com>
2026-06-18 12:50:49 +0700

    MỚi thêm

--- Webbanhang/Program.cs
+++ Webbanhang/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Webbanhang.Models;
 using Webbanhang.Repositories;
@@ -8,39 +8,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-         .AddDefaultTokenProviders()
-         .AddDefaultUI()
-         .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+    .AddDefaultTokenProviders()
+    .AddDefaultUI()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();     
+
 
 builder.Services.AddScoped<IProductRepository, EFProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, EFCategoryRepository>();
-//builder.Services.AddSingleton<IProductRepository,MockProductRepository>();
-//builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
-{
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 
-app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();     
 
 app.Run();
